c3d2aa420cfe59c07f887aa413ffe553b1bfd50d
Ella77 <ella@mykoon.com>
2018-10-13 11:50:53 +0900

    first commit

--- baseball/urls.py
+++ baseball/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name= 'login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name= 'logout'),
+
 ]
